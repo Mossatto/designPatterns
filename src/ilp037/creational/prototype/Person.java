cce4b29c844ce15e60c4aef77467314852e3c43b
Matheus Mossatto <matheusmossatto.dosantos@gmail.com>
2024-04-03 19:40:34 -0300

    ol

--- src/ilp037/creational/prototype/Person.java
+++ src/ilp037/creational/prototype/Person.java
@@ -2,6 +2,16 @@ package ilp037.creational.prototype;
 
 public class Person extends Prototype<Person> implements Cloneable{
 
+    String name;
+    Long document;
 
 
+    public Person(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Long Time");
+    }
 }
